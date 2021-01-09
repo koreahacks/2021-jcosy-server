60ae3429de1ea95cd2090dc269e7ab8fa1bda03d
meanjung <meanjunghong@naver.com>
2021-01-09 17:51:04 +0900

    update : quest controller

--- trolley_server/routes/main.js
+++ trolley_server/routes/main.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const upload = require('../modules/multer');
-
 const QuestController = require('../controllers/questController');
+const authMiddleware = require('../middleware/auth');
 
 router.post('/quest/register', upload.single('image'), QuestController.register);
+
 module.exports = router;
 
