86034251140616c887023b72aabc3236333d75ae
rkdls8268 <rkdls8268@gmail.com>
2021-01-09 17:48:28 +0900

    delete files

--- trolley_server/controllers/userController.js
+++ trolley_server/controllers/userController.js
@@ -5,23 +5,7 @@ const util = require('../modules/util');
 const jwt = require('../modules/jwt');
 
 const user = {
-    verify: async (req, res) => {
-        const {
-            userIdx
-        } = req.body;
-
-        const {
-            token,
-            _
-        } = await jwt.sign(userIdx);
-
-        // 로그인이 성공적으로 마쳤다면 - LOGIN_SUCCESS 전달
-        res.status(CODE.OK)
-            .send(util.success(CODE.OK, MSG.LOGIN_SUCCESS, {
-                accessToken: token
-                //, refreshToken: refreshToken
-            }));
-    },
+ 
 }
 
 module.exports = user;
